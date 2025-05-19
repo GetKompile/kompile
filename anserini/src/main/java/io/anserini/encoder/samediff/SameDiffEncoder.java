6ebdd32a0e5bb722138031d188d3c56b3f648eba
agibsonccc <adam@konduit.ai>
2025-05-19 12:12:17 +0900

    WIP update

--- anserini/src/main/java/io/anserini/encoder/samediff/SameDiffEncoder.java
+++ anserini/src/main/java/io/anserini/encoder/samediff/SameDiffEncoder.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2025 Kompile Inc.
- * *
- * * Licensed under the Apache License, Version 2.0 (the "License");
- * * you may not use this file except in compliance with the License.
- * * You may obtain a copy of the License at
- * *
- * * http://www.apache.org/licenses/LICENSE-2.0
- * *
- * * Unless required by applicable law or agreed to in writing, software
- * * distributed under the License is distributed on an "AS IS" BASIS,
- * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * * See the License for the specific language governing permissions and
- * * limitations under the License.
+ *  Copyright 2025 Kompile Inc.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  */
 
 package io.anserini.encoder.samediff;
@@ -24,14 +24,14 @@ import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.nd4j.autodiff.samediff.SameDiff;
-import org.nd4j.samediff.frameworkimport.onnx.OnnxFrameworkImporter; // Added import
-
+import org.nd4j.samediff.frameworkimport.onnx.importer.OnnxFrameworkImporter;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -105,7 +105,7 @@ public abstract class SameDiffEncoder<T> implements AutoCloseable {
             // For now, we assume default import behavior.
             // The outputTensorNamesFromModel passed to this constructor will be used in subclasses
             // to fetch the outputs from the SameDiff graph. Ensure they are valid.
-            this.sameDiffModel = importer.runImport(onnxModelPath.toFile().getAbsolutePath(), true, (Map<String,String>) null);
+            this.sameDiffModel = importer.runImport(onnxModelPath.toFile().getAbsolutePath(), Collections.emptyMap(),true,true);
 
             if (this.sameDiffModel == null) {
                 throw new IOException("Failed to import ONNX model to SameDiff from " + onnxModelPath + ". Importer returned null.");
