0849ee6903cede2ad5a943bbb50798a271b5ba69
agibsonccc <adam@konduit.ai>
2025-05-22 09:59:16 +0900

    add no op chunker

---
+++ kompile-app/kompile-app-core/src/main/java/ai/kompile/app/core/chunking/NoOpChunker.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Kompile Inc.
+ * *
+ * * Licensed under the Apache License, Version 2.0 (the "License");
+ * * you may not use this file except in compliance with the License.
+ * * You may obtain a copy of the License at
+ * *
+ * * http://www.apache.org/licenses/LICENSE-2.0
+ * *
+ * * Unless required by applicable law or agreed to in writing, software
+ * * distributed under the License is distributed on an "AS IS" BASIS,
+ * * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * * See the License for the specific language governing permissions and
+ * * limitations under the License.
+ */
+
+package ai.kompile.app.core.chunking;
+
+import org.springframework.ai.document.Document;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component("recursiveCharacterTextChunker")
+@ConditionalOnProperty(matchIfMissing = true)
+public class NoOpChunker implements TextChunker {
+    @Override
+    public List<Document> chunk(Document document, Map<String, Object> options) {
+        return Arrays.asList(document);
+    }
+
+    @Override
+    public String getName() {
+        return "noop";
+    }
+}
